c60db825cd5108732d0daa55e94b5827b829c7ae
Luke Pratley <Luke.Pratley@gmail.com>
2020-05-09 19:11:16 -0400

    added a qu fitting example with mcmc

--- faradaydreams/qu_fitting.py
+++ faradaydreams/qu_fitting.py
@@ -3,17 +3,26 @@ from scipy.optimize import minimize
 import numpy as np
 
 
-def log_likelihood(params, model, channels, y, sigma):
-    return -0.5 * np.sum((y - model(channels, params)) ** 2 / sigma**2)
+def log_probability(params, model, lambda2, y, sigma, constraint):
+    prior = 0.
+    return -0.5 * np.sum(np.abs(y - model(lambda2, *params)) ** 2 / sigma**2) + prior
 
 
-def mcmc_sample(channels, y, sigma, start_params, log_probability, nwalkers, iters):
+def log_prior(params, constraint):
+    if constraint(*params):
+        return -np.infty
+    else:
+        return 0
+
+
+def mcmc_sample(model, lambda2, y, sigma, start_params, constraint, nwalkers, iters):
     ndim = len(start_params)
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(channels, y, sigma))
-    sampler.run_mcmc(start_params + 1e-4 * np.random.randn(nwalkers, ndim), iters, progress=True)
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(model, lambda2, y, sigma, constraint))
+    sampler.run_mcmc(start_params + 100 * np.random.randn(nwalkers, ndim), iters, progress=True)
+    return sampler
 
 
-def least_squares(params_estimate, model, channels, y, sigma):
-    nll = lambda *args: -log_likelihood(*args)
+def least_squares(params_estimate, model, lambda2, y, sigma):
+    nll = lambda *args: -log_probability(*args)
     initial = params_estimate
-    return minimize(nll, initial, args=(channels, y, sigma))
+    return minimize(nll, initial, args=(model, lambda2, y, sigma, None), method='BFGS', tol=1e-6, options={'maxiter': 1e5, 'disp':True})
